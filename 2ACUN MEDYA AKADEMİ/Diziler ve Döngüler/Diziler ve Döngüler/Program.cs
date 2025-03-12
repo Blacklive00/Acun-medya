65e90db2fdb6ba8a1df402e22c00f5612eee09d6
Blacklive00 <102056225+Blacklive00@users.noreply.github.com>
2025-03-13 01:25:53 +0300

    acun-medya
    proje hazır hale geldi

---
+++ 2ACUN MEDYA AKADEMİ/Diziler ve Döngüler/Diziler ve Döngüler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] sayilar = new double[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Lütfen {0}. sayıyı girin: ", i + 1);
+            sayilar[i] = Convert.ToDouble(Console.ReadLine());
+        }
+        double toplam = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            toplam += sayilar[i];
+        }
+        double ortalama = toplam / 5;
+
+        Console.WriteLine("Sayıların Ortalaması: " + ortalama);
+    }
+}
